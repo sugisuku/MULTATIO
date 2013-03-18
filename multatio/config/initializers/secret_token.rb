013647936254de916e3a601e8497fafc9d6d8b77
shinriyo <shinriyo@gmail.com>
2013-03-18 10:40:44 +0900

    remove non3 rails

--- multatio/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Multatio::Application.config.secret_token = 'b91259d32b107df99e43c67c1eb9220e9ed53dab000aa6422519a868151472dd98840ecca828a3e59adce93a5c43e8e6031d465c6efed36cfb27aca8eb73b079'
