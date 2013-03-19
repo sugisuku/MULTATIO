4140c20a36264ddef28c630c805085adb579452a
shinriyo <shinriyo@gmail.com>
2013-03-19 10:02:58 +0900

    root設定をbooksへ

--- multatio/config/routes.rb
+++ multatio/config/routes.rb
@@ -1,5 +1,6 @@
 Multatio::Application.routes.draw do
   resources :books
+  root 'books#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
