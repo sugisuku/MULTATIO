1e0157be1d3a864eea69c6f93eae2dc094c473a3
sugisuku <sugita@oneofthem.jp>
2013-03-20 15:09:09 +0900

    未入力チェック追加で冗長なのを削除

--- multatio/app/models/book.rb
+++ multatio/app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ActiveRecord::Base
   scope :titled, ->(q) { where 'title like ?', "%#{q}%"}
   validates_numericality_of :price, :greater_than => 0 
   validates :url, :format => URI::regexp(%w(http https))
-  validates_presence_of :severity, :null => false 
-  validates_presence_of :reason, :null => false 
-  validates_presence_of :country, :null => false 
+  validates_presence_of :title
+  validates_presence_of :severity
+  validates_presence_of :reason
+  validates_presence_of :country
 end
