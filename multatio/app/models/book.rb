74377f8b73bd7a75fb62cc6a29f87bd923d1f9a2
sugisuku <sugita@oneofthem.jp>
2013-03-20 15:08:00 +0900

    未入力チェック追加とプルダウンで購入種類

--- multatio/app/models/book.rb
+++ multatio/app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ActiveRecord::Base
   scope :titled, ->(q) { where 'title like ?', "%#{q}%"}
   validates_numericality_of :price, :greater_than => 0 
   validates :url, :format => URI::regexp(%w(http https))
+  validates_presence_of :severity, :null => false 
+  validates_presence_of :reason, :null => false 
+  validates_presence_of :country, :null => false 
 end
