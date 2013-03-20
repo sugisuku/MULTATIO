3814fa9d42150d3c10dd0530db984016bf3be491
sugisuku <sugita@oneofthem.jp>
2013-03-20 13:12:16 +0900

    バリデートと基本haml修正

--- multatio/app/models/book.rb
+++ multatio/app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
   scope :titled, ->(q) { where 'title like ?', "%#{q}%"}
-  #validates :prise, :greater_than => 0 
+  validates_numericality_of :price, :greater_than => 0 
   #validates :url, :format => URI::regexp(%w(http https))
 end
