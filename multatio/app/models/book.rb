e32379127268108d2101223ec186ead78c55361d
sugisuku <sugita@oneofthem.jp>
2013-03-21 10:29:00 +0900

    book model順変えた

--- multatio/app/models/book.rb
+++ multatio/app/models/book.rb
@@ -1,10 +1,11 @@
 class Book < ActiveRecord::Base
   extend ActiveModel::Translation
   scope :titled, ->(q) { where 'title like ?', "%#{q}%"}
+
+  validates_presence_of :reason, :greater_than => 0
   validates_numericality_of :price, :greater_than => 0
   validates :url, :format => URI::regexp(%w(http https))
-  validates_presence_of :title
   validates_presence_of :severity
-  validates_presence_of :reason
+  validates_presence_of :title
   validates_presence_of :country
 end
