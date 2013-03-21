85849b5f4ff188790777c87cf44948e5bc34f32f
sugisuku <sugita@oneofthem.jp>
2013-03-22 02:04:50 +0900

    各種ビュー追加と要らないDestroyビュー削除

--- multatio/app/controllers/users_controller.rb
+++ multatio/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -36,5 +38,16 @@ class UsersController < ApplicationController
       format.html { redirect_to user_url }
       format.json { head :no_content }
     end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:provider, :uid, :name, :screen_name)
   end
 end
