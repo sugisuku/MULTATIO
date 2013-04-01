ce2f90cadb0b59ad167b34a7f348c302b3640fd6
sugisuku <sugita@oneofthem.jp>
2013-04-01 19:54:19 +0900

    willnetさんにチェックしてもらいために一旦コミット

--- multatio/app/controllers/routes_controller.rb
+++ multatio/app/controllers/routes_controller.rb
@@ -4,7 +4,8 @@ class RoutesController < ApplicationController
 #    @route = Train.find(params[:train_id]).routes.create(params[:route])
     train = Train.find(params[:train_id])
     routes = train.routes
-    routes.create(params[:new_route]) #can't :route?
+    #@route = routes.create(params[:route]) #can't :route?
+    @route = routes.create(params[:new_route])
     redirect_to train_path(@train)
   end
 end
